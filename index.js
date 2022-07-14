125c84d9aa96cc12bd569e6b893215b59a04d45f
daviswcci <79462946+daviswcci@users.noreply.github.com>
2022-07-14 12:01:30 -0400

    finished game and styled it

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let snakeDir = 1; // 0,1,2,3 ... N,S,E,W
 let snakeLen = 1;
 // NOTE: add a display text for our snake's direction
 let score = 0;
+let highscore = 0;
 
 let fruitPosn = {x: 5, y: 3} // fruitposn is an object with properties x and y
 
@@ -30,18 +31,20 @@ function DrawFruit(){
     // grab the html element where our fruit is located
     let fruitSpaceHTML = document.getElementById(fruitPosn.x + "," + fruitPosn.y);
     // update its visuals
-    fruitSpaceHTML.innerText = "🍎";
+    fruitSpaceHTML.innerText = "🍬";
     // done.
 }
 RandomizeFruitPosn();
 DrawFruit();
 
 function DrawSnake(){
-    snakeBody.forEach((segment) => {
-        let snakeSpaceHTML = document.getElementById(segment.x + "," + segment.y);
+    for(let i = 0; i < snakeBody.length; i++){
+        let snakeSpaceHTML = document.getElementById(snakeBody[i].x + "," + snakeBody[i].y);
         // update its visuals
-        snakeSpaceHTML.innerText = "🟢";
-    });
+
+        snakeSpaceHTML.innerText = i == 0 ? "💀" : "🎃";
+    }
+
 }
 DrawSnake();
 
@@ -49,4 +52,137 @@ DrawSnake();
 function RandomizeFruitPosn(){
     fruitPosn.x = Math.floor(Math.random() * gridWidth);
     fruitPosn.y = Math.floor(Math.random() * gridHeight);
+}
+
+function MoveSnake(){
+    // create a new head for our snake
+    let newSnakeHead = {x:false, y:false}
+    switch(snakeDir){
+        case 0: // NORTH
+            newSnakeHead.x = snakeBody[0].x;
+            newSnakeHead.y = snakeBody[0].y - 1;
+            break;
+        case 1: // SOUTH
+            newSnakeHead.x = snakeBody[0].x;
+            newSnakeHead.y = snakeBody[0].y + 1;
+            break;
+        case 2: // EAST
+            newSnakeHead.x = snakeBody[0].x + 1;
+            newSnakeHead.y = snakeBody[0].y;
+            break;
+        case 3: // WEST
+            newSnakeHead.x = snakeBody[0].x - 1;
+            newSnakeHead.y = snakeBody[0].y;
+            break;
+    }
+    // handle the case when our snake goes off the board.
+    if(newSnakeHead.x < 0){
+        newSnakeHead.x += gridWidth;
+    }
+    if(newSnakeHead.y < 0){
+        newSnakeHead.y += gridHeight;
+    }
+    // wrap around.
+    newSnakeHead.x = newSnakeHead.x % gridWidth;
+    newSnakeHead.y = newSnakeHead.y % gridHeight;
+    // add the new head to our body
+    snakeBody.unshift(newSnakeHead);
+    //console.log(snakeBody);
+    // check if fruit coordinate exists on our snake
+    let grow = false;
+    for(let i = 0; i < snakeBody.length; i++){
+        grow = snakeBody[i].x == fruitPosn.x && snakeBody[i].y == fruitPosn.y
+        if(grow){ //grow is true, break out of the loop
+            clearInterval(gameInterval);
+            gameInterval = setInterval(PlayGame, 200 - (score));
+            snakeLen++;
+            score++;
+            RandomizeFruitPosn();
+            break;
+        }
+    }
+    if(!grow){
+        //remove our tail*
+        snakeBody.pop(); // removes the last item from our array
+    }
+    // check if newsnakehead already exists on our snake
+    let die = false;
+    for(let i = 1; i < snakeBody.length; i++){
+        die = snakeBody[i].x == newSnakeHead.x && snakeBody[i].y == newSnakeHead.y
+        //console.log(die);
+        if(die){ //grow is true, break out of the loop
+            break;
+        }
+    }
+    if(die){
+        ResetGame();
+    }
+}
+// need a way to run this over time
+function PlayGame(){
+    // update our logic
+    MoveSnake();
+    // draw our game
+    ClearGrid();
+    DrawFruit();
+    DrawSnake();
+    DrawScore();
+}
+
+// Intervals
+// intervals are timers. they run functions every few milliseconds
+// if we set multiple timers, code will run multiple times
+// in order to stop a timer, we use the clearInterval() method
+// in order to create a timer, we use the setIntverval() method
+// with set interval, we specify how many milliseconds in between each run and the function to run
+let gameInterval = setInterval(PlayGame, 200);
+
+
+
+// need a way to change directions
+document.addEventListener("keypress", (event) => {
+    if(event.code == "KeyA" && snakeDir != 2){
+        snakeDir = 3;
+    }
+    else if(event.code == "KeyW" && snakeDir != 1){
+        snakeDir = 0;
+    }
+    else if(event.code == "KeyS" && snakeDir != 0){
+        snakeDir = 1;
+    }
+    else if(event.code == "KeyD" && snakeDir != 3){
+        snakeDir = 2;
+    }
+    //console.log(snakeDir);
+});
+
+// die / reset game function
+function ResetGame(){
+    if(score > highscore){
+        highscore = score; // this updates our high score if we beat it
+        let leaderboard = document.getElementById("highscore"); // grabs our score element
+        leaderboard.innerText = "High Score: " + highscore;
+    }
+    snakeBody = [ {x:0,y:0} ];
+    snakeDir = 1; // 0,1,2,3 ... N,S,E,W
+    snakeLen = 1;
+    score = 0;
+    RandomizeFruitPosn();
+    clearInterval(gameInterval);
+    gameInterval = setInterval(PlayGame, 200);
+}
+
+function DrawScore(){
+    // fill in later
+    let scoreboard = document.getElementById("score"); // grabs our score element
+    scoreboard.innerText = "Score: " + score;
+}
+
+function ClearGrid(){
+    for(let i = 0; i < gridHeight; i++){
+        for(let j = 0; j < gridWidth; j++){
+            let gridSpaceHTML = document.getElementById( j + "," + i);
+            gridSpaceHTML.innerText = "";
+        }
+    }
 }
